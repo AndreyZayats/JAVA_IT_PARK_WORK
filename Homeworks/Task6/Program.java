c0a6f8abcbeca97fa88e751a183a2e55d00f5ca9
AndreyZayats <zayatsasprogramming@yandex.ru>
2017-03-09 17:16:05 +0300

    add homework

--- Homeworks/Task6/Program.java
+++ Homeworks/Task6/Program.java
@@ -3,20 +3,22 @@ import java.util.Scanner;
 public class Program {
 	public static void main(String[] args) {
 		int[] a = new int[10];
-		int result = 0;
+		int result = 1;
 
 		Scanner scanner = new Scanner(System.in);
 
 		for(int i = 0; i < a.length; i++) {
 			int b = scanner.nextInt();
-			
 			if(b == -1) {
 				break;
 			}
 
 			a[i] = b;
 
-			result += a[i];
+			if(i%2 == 1 & a[i]%2 == 0) {
+				
+				result *= a[i];
+			}
 		}
 
 		System.out.println(result);
