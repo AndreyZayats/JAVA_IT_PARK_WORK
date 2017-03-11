1d4e628164bddd9304a0e07c7ba67cb8827dfab3
zayatsasprogramming <zayatsasprogramming@yandex.ru>
2017-03-11 17:34:49 +0300

    add classwork

--- Classworks/Task1/Program.java
+++ Classworks/Task1/Program.java
@@ -1,12 +1,17 @@
+import java.util.Scanner;
+
 public class Program {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
 		int a[] = {-3, 10, 11, 12, 15, -5};
 
 		int min = a[0];
 		int max = a[0];
 		int minIndex = 0;
 		int maxIndex = 0;
-		
+		int indexX = -1;
+		int x = scanner.nextInt();
 
 		for(int i = 1; i < a.length; i++) {
 			if(min > a[i]) {
@@ -26,8 +31,13 @@ public class Program {
 		a[minIndex] = a[maxIndex];
 		a[maxIndex] = temp;
 
-		for(int b : a) {
-			System.out.println(b);
+		for(int i = 0; i<a.length; i++) {
+			
+			if(a[i] == x) {
+				indexX = i;
+			}
 		}
+
+		System.out.println(indexX);
 	}
 }
