a5ac0079a9f5505cc1ab0bbb4fb9e5abdb2bb0af
AndreyZayats <zayatsasprogramming@yandex.ru>
2017-03-14 15:41:36 +0300

    add homework

---
+++ Homeworks/Task14/Program.java
@@ -0,0 +1,25 @@
+import java.util. Scanner;
+
+class Program {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		while(true) {
+			int a = scanner.nextInt();
+			if(a < 1) {
+				break;
+			}
+			System.out.println(isPrime(a));
+		}
+	}
+
+	public static boolean isPrime(int n) {
+		for(int i = 2; i * i <= n; i++) {
+			int a = n%i;
+			if(a == 0) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+}
