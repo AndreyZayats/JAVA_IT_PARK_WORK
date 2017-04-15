4a2e474fa9afc0c29f129031cb8912f8b20ffb74
AndreyZayats <zayats89093089250>
2017-04-15 17:47:05 +0300

    add classwork

--- Classworks/Task10/src/ru/itpark/work/Main.java
+++ Classworks/Task10/src/ru/itpark/work/Main.java
@@ -1,7 +1,6 @@
 package ru.itpark.work;
 
-import java.io.FileOutputStream;
-import java.io.OutputStream;
+import java.io.*;
 
 public class Main  {
 
@@ -12,10 +11,12 @@ public class Main  {
         Triangle triangle2 = new Triangle(5, 7, 6);
 
         OutputStream outputStream = new FileOutputStream("output.txt");
+
         String sq1= square1.toString(square1);
         String sq2 = square2.toString(square2);
         String tr1 = triangle1.toString(triangle1);
         String tr2 = triangle2.toString(triangle2);
+
         byte description1[] = sq1.getBytes();
         byte description2[] = sq2.getBytes();
         byte description3[] = tr1.getBytes();
@@ -25,5 +26,19 @@ public class Main  {
         outputStream.write(description2);
         outputStream.write(description3);
         outputStream.write(description4);
+
+        try {
+            InputStream inputStream = new FileInputStream("output.txt");
+            byte bytesFromFile[] = new byte[200];
+            inputStream.read(bytesFromFile);
+
+            for (int i = 0; i < bytesFromFile.length; i++) {
+                System.out.print((char) bytesFromFile[i]);
+            }
+        } catch(FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (IOException e) {
+            System.out.println("Проблемы с чтением");
+        }
     }
 }
